ccec39a2a2431ff1878825ade9512b0b8f89dc94
Gabriel de Andrade Cordeiro <gabriel.cordeiro@roit.com.br>
2020-06-03 16:23:24 -0300

    Initial Commit

---
+++ MoviesDB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesDB
+//
+//  Created by NoteROIT on 03/06/20.
+//  Copyright © 2020 Notebook. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
